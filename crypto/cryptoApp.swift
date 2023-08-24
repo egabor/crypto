57976e803127f96ee45d44b2c1f173a18f74a29f
Eszenyi Gábor <eszenyi.gabor@icloud.com>
2023-08-24 13:27:26 +0200

    Initial Commit

---
+++ crypto/cryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  cryptoApp.swift
+//  crypto
+//
+//  Created by Eszenyi Gábor on 2023. 08. 24..
+//
+
+import SwiftUI
+
+@main
+struct cryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
